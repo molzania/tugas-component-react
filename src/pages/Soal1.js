c9eb6b1724702af7616e28f649ba73ca73dda123
molzania <molzania_1507@yahoo.co.id>
2021-08-22 12:04:39 +0700

    commit on 'main'

--- src/pages/Soal1.js
+++ src/pages/Soal1.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import './Soal1.css';
+import {useState} from 'react';
+import img1 from './pink.jpg';
 
 function Soal1() {
+    const [count, setCount] = useState(0);
+
+    const tambah = () => {
+      if(count >= 0){
+setCount(count + 1);
+      }
+    }
+
+    const kurang = () => {
+      if(count > 0){
+        setCount(count-1);
+      } else (
+        setCount(0)
+      )
+    }
+
     return (
-      <div className="Home">
-    <h3>This is first page</h3>
-  
+      <div className="Soal-1">
+    <div className="pertanyaan-1" style={{backgroundImage: `url(${img1})`}}>
+    <h1>Soal 1</h1>
+    <p>Buatlah component counter tanpa melihat kodingan siapapun</p>
+    </div>
+    <div className="jawaban-1">
+    <p>Masukkan angka yang diinginkan di bawah ini:</p>
+    <h4>Angka</h4>
+    <div className="counter">
+      <button onClick={tambah}>+</button>
+        <span className="angka">{count}</span>
+      <button onClick={kurang}>-</button>
+      </div>
+      </div>
       </div>
     );
   }
